0c68174f0fe9d8b677ffc655622d15e7cf2ef625
nikita titarenko <nt@poloniumarts.com>
2018-06-01 10:44:10 +0300

    + trace MongoDB requests

--- SurveyBot.Core/Models/MongoSettings.cs
+++ SurveyBot.Core/Models/MongoSettings.cs
@@ -9,5 +9,10 @@ namespace SurveyBot.Core.Models
     {
         public string ConnectionString { get; set; }
         public string Database { get; set; }
+
+        public string Server { get; set; }
+        public int Port { get; set; }
+        public string UserName { get; set; }
+        public string Password { get; set; }
     }
 }
--- SurveyBot.Core/SurveyContext.cs
+++ SurveyBot.Core/SurveyContext.cs
@@ -1,7 +1,11 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using Microsoft.Extensions.Options;
+using MongoDB.Bson;
 using MongoDB.Driver;
+using MongoDB.Driver.Core.Events;
 using MongoDB.Driver.Linq;
 using SurveyBot.Core.Models;
 using SurveyBot.Core.Models.Domain;
@@ -14,7 +18,18 @@ namespace SurveyBot.Core
 
         public SurveyContext(IOptions<MongoSettings> settings)
         {
-            var client = new MongoClient(settings.Value.ConnectionString);
+            //var client = new MongoClient(settings.Value.ConnectionString);
+            var client = new MongoClient(new MongoClientSettings()
+            {
+                Server = new MongoServerAddress(settings.Value.Server, settings.Value.Port),
+                Credential = MongoCredential.CreateCredential(settings.Value.Database, settings.Value.UserName, settings.Value.Password),
+                ClusterConfigurator = builder =>
+                {
+                    builder.Subscribe(new SingleEventSubscriber<CommandStartedEvent>(TraceRequest));
+                    builder.Subscribe(new SingleEventSubscriber<CommandSucceededEvent>(TraceResponse));
+                }
+            });
+
             _mongo = client.GetDatabase(settings.Value.Database);
 
             if (_mongo == null)
@@ -34,6 +49,24 @@ namespace SurveyBot.Core
 
         public IMongoCollection<Counter> Counters
             => _mongo.GetCollection<Counter>("Counters");
+
+        void TraceRequest(CommandStartedEvent command)
+        {
+            if (new List<string> { "isMaster", "buildInfo", "saslStart", "saslContinue", "getLastError" }.Contains(command.CommandName)) return;
+
+            Debug.WriteLine("**********************************************");
+            Debug.WriteLine($"{command.CommandName}");
+            Debug.WriteLine($"{command.Command.ToJson()}");
+        }
+
+        void TraceResponse(CommandSucceededEvent command)
+        {
+            if (new List<string> { "isMaster", "buildInfo", "saslStart", "saslContinue", "getLastError" }.Contains(command.CommandName)) return;
+
+            Debug.WriteLine("**********************************************");
+            Debug.WriteLine($"{command.CommandName}");
+            Debug.WriteLine($"{command.Reply.ToJson()}");
+        }
 
     }
 }
